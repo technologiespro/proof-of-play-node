4288bc53a2987db4e53994e5057a3ee2c59dcded
technolog <technologies.bro@gmail.com>
2019-09-07 15:19:18 +0300

    add apiRouter in app

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
+var apiRouter = require('./routes/api');
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 
@@ -19,7 +20,8 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', indexRouter);
+app.use('/api', indexRouter);
+app.use('/', apiRouter);
 app.use('/users', usersRouter);
 
 // catch 404 and forward to error handler

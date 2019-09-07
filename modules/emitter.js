cdb72c6d0de11993979a4c3efc9ee442163ee43f
technolog <technologies.bro@gmail.com>
2019-09-07 15:11:20 +0300

    emitter

---
+++ modules/emitter.js
@@ -0,0 +1,19 @@
+var util = require('util')
+var eventEmitter = require('events').EventEmitter
+
+function Event () {
+    eventEmitter.call(this)
+}
+
+util.inherits(Event, eventEmitter)
+
+Event.prototype.sendEvent = function(type, data) {
+    this.emit(type, data)
+}
+var eventBus = new Event();
+eventBus.setMaxListeners(100)
+
+module.exports = {
+    emitter : Event,
+    eventBus : eventBus
+};
